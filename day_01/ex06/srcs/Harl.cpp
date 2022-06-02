564ebe48630890322658c2b0e2defc56a3aadb1b
Noe DORMOY <ndormoy@e3r5p12.clusters.42paris.fr>
2022-06-02 14:41:21 +0200

    Fix Day01

--- day_01/ex06/srcs/Harl.cpp
+++ day_01/ex06/srcs/Harl.cpp
@@ -4,36 +4,34 @@
 
 Harl::Harl(void)
 {
-	// std::cout << "Constructor called" << std::endl;
 }
 
 /*-------------------Destructor----------------------*/
 
 Harl::~Harl(void)
 {
-	// std::cout << "Destructor called" << std::endl;
 }
 
 /*-------------------PRIVATE FUNCTIONS---------------*/
 
 void	Harl::_debug(void)
 {
-	std::cout << "[ DEBUG ]\n" << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "[ DEBUG ]\n" << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n" << std::endl;
 }
 
 void	Harl::_info(void)
 {
-	std::cout << "[ INFO ]\n" << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+	std::cout << "[ INFO ]\n" << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n" << std::endl;
 }
 
 void	Harl::_warning(void)
 {
-	std::cout << "[ WARNING ]\n" << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]\n" << "I think I deserve to have some extra bacon for free.\nI've been coming for years whereas you started working here since last month.\n" << std::endl;
 }
 
 void	Harl::_error(void)
 {
-	std::cout << "[ ERROR ]\n" << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]\n" << "This is unacceptable! I want to speak to the manager now.\n" << std::endl;
 }
 
 /*-------------------PUBLIC FUNCTIONS----------------*/
@@ -62,21 +60,3 @@ void	Harl::complain(std::string level)
 	}
 	std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
-
-/* void	Harl::complain(std::string level)
-{
-	int	yes = 0;
-	std::string str[5] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void (Harl::*fct[5])(void) = {&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
-
-	for (int i = 0; i < 4; i++)
-	{
-		if (level.compare(str[i]) == 0 || yes == 1)
-		{
-			yes = 1;
-			(this->*fct[i])();
-		}
-	}
-	if (yes == 0)
-		std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
-} */
--- day_01/ex06/srcs/main.cpp
+++ day_01/ex06/srcs/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 
 	if (argc != 2)
 	{
-		std::cerr << "Expected : ./harl <LEVEL>" << std::endl;
+		std::cerr << "Expected : ./harlFilter <LEVEL>" << std::endl;
 		return (1);
 	}
 	if (std::strlen(argv[1]) == 0)
