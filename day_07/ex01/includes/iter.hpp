53ad42f4798609f74228f1705118c5279b8b8c8c
parallels <ndormoy@student.42.fr>
2022-06-21 11:38:48 +0200

    Day07 ex01 Ok

--- day_07/ex01/includes/iter.hpp
+++ day_07/ex01/includes/iter.hpp
@@ -1,6 +1,37 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
+#include <iostream>
+#include <string>
+
+template<typename T>
+void	__double(T &i)
+{
+	int	*tmp = const_cast<int *>(&i);
+	*tmp *= 2;
+}
+
+template<typename T, typename U>
+void	iter(T tab[],  U len, void function(T const &))
+{
+	for (int i = 0; i < len; i++)
+	{
+		function(tab[i]);
+	}
+}
+
+// class Awesome
+// {
+// 	public:
+// 	Awesome( void ) : _n( 42 ) { return; }
+// 	int get( void ) const { return this->_n; }
+// 	private:
+// 	int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+// template< typename T >
+// void print( T const & x ) { std::cout << x << std::endl; return; }
 
 
 #endif
--- day_07/ex01/srcs/main.cpp
+++ day_07/ex01/srcs/main.cpp
@@ -1,1 +1,22 @@
 #include "iter.hpp"
+
+int main()
+{
+	int tab[4] = {2, 4, 6, 8};
+	std::cout << "Before : " << std::endl;
+	for (int i = 0; i < 4; i++)
+		std::cout << tab[i] << std::endl;
+	iter(tab, 4, __double);
+	std::cout << "After : " << std::endl;
+	for (int i = 0; i < 4; i++)
+		std::cout << tab[i] << std::endl;
+}
+
+// int main() 
+// {
+// 	int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+// 	Awesome tab2[5];
+// 	iter( tab, 5, print );
+// 	iter( tab2, 5, print );
+// 	return 0;
+// }
